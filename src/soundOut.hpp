e66744d2845642e78550b1bf41b2672adef042f1
KIRIAKE Masanori <seiken@rd01.net>
2021-04-14 21:52:10 +0900

    Add: 	* M5シリーズのスピーカーピンをdefine 	* begin()を使いやすくした。

--- src/soundOut.hpp
+++ src/soundOut.hpp
@@ -1,8 +1,13 @@
 #ifndef _SOUNDOUT_HPP_
 #define _SOUNDOUT_HPP_
+#include <_types/_uint8_t.h>
 #include <cstdint>
 #include "Arduino.h"
 #include "MorseTable.h"
+
+#define M5STICK_C_PLUS 2
+#define M5STICK_HAT 26
+#define M5STACK_SPEAKER 26
 
 struct beepCmd {
   uint8_t type;
@@ -12,6 +17,8 @@ struct beepCmd {
 
 class SoundOut {
 public:
+  int begin() { return begin(2, 100); }
+  int begin(uint8_t pin) { return begin(pin, 100); }
   int begin(uint8_t pin, uint8_t volume);
   void beepOut(uint16_t freq, uint16_t length);
   void morseOut(const char* s);
